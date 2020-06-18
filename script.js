ab68e0efa1a004b5c24449955370d1ed82292bf8
c-obsesie <cosmin.mihalache95.com>
2020-06-18 10:42:16 +0300

    Signed-off-by: c-obsesie <cosmin.mihalache95.com>

--- script.js
+++ script.js
@@ -20,15 +20,15 @@ function initMap() {
   });
   
   var sector1Coords=[
-    {lat:44.487641,lng:  26.105851},
-    {lat: 44.438210,lng:26.101234},
+    {lat:44.434798,lng: 26.097717},
+    {lat: 44.466941,lng:26.097150},
     {lat:44.441327,lng: 26.198153},
     {lat: 44.457841,lng: 26.154109},
     {lat: 44.479183,lng: 26.181571},
   ]
   var sector2Coords=[  
-    {lat:44.430037,lng:  26.096058},
-    {lat:44.4253560,lng:26.108394},
+    {lat:44.434798,lng: 26.097717},
+    {lat:44.466941,lng:26.097150},
     {lat:44.423498,lng: 26.198153},
     {lat:44.409672,lng: 26.123571},
     {lat:44.393841,lng:26.179947}, 
@@ -36,14 +36,14 @@ function initMap() {
   
   ] 
    var sector3Coords=[
-    {lat:44.487641,lng:  26.105851},
+    {lat:44.434798,lng: 26.097717},
     {lat: 44.438210,lng:26.101234},
     {lat:44.441327,lng: 26.198153},
     {lat: 44.457841,lng: 26.154109},
     {lat: 44.479183,lng: 26.181571},
   ]
   var sector4Coords=[  
-    {lat:44.430037,lng:  26.096058},
+    {lat:44.434798,lng: 26.097717},
     {lat:44.4253560,lng:26.108394},
     {lat:44.423498,lng: 26.198153},
     {lat:44.409672,lng: 26.123571},
@@ -52,14 +52,14 @@ function initMap() {
   
   ]
   var sector5Coords=[
-    {lat:44.487641,lng:  26.105851},
+    {lat:44.434798,lng: 26.097717},
     {lat: 44.438210,lng:26.101234},
     {lat:44.441327,lng: 26.198153},
     {lat: 44.457841,lng: 26.154109},
     {lat: 44.479183,lng: 26.181571},
   ]
   var sector6Coords=[  
-    {lat:44.430037,lng:  26.096058},
+    {lat:44.434798,lng: 26.097717},
     {lat:44.4253560,lng:26.108394},
     {lat:44.423498,lng: 26.198153},
     {lat:44.409672,lng: 26.123571},
@@ -92,6 +92,8 @@ function initMap() {
   // Draw the polygon on the desired map instance
   sector1.setMap(map);
   sector2.setMap(map);
+  sector3.setMap(map);
+  sector4.setMap(map);
   // The marker, positioned at Uluru
   var marker = new google.maps.Marker({position: uluru, map: map});
 }
